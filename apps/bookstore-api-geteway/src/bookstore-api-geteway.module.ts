4ad9adce549289db9ec24255cff00f89d11eb38a
Mahmoud Reda Shaaban <mahreda19@gmail.com>
2025-05-25 19:46:00 +0300

    add Service books and add in app getway and test

--- apps/bookstore-api-geteway/src/bookstore-api-geteway.module.ts
+++ apps/bookstore-api-geteway/src/bookstore-api-geteway.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookstoreApiGetewayController } from './bookstore-api-geteway.controller';
 import { BookstoreApiGetewayService } from './bookstore-api-geteway.service';
 import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BooksModule],
   controllers: [BookstoreApiGetewayController],
   providers: [BookstoreApiGetewayService],
 })
